5d86c82e073d9e8d482c02befa4e0e2f2722f4a0
Matthew <sshadow.230701@gmail.com>
2021-06-24 19:13:00 +1200

    Ui updates

--- app/src/main/java/com/example/stop_loafing_around/Show_recipe.kt
+++ app/src/main/java/com/example/stop_loafing_around/Show_recipe.kt
@@ -33,8 +33,36 @@ class Show_recipe : AppCompatActivity() {
         LoadRecipe().getRecipeData(Recipe_to_load.recipe_name,steps_recycler?.adapter,ingredients_recycler?.adapter,this,this)
         binding.createRecipeName.text = Recipe_to_load.recipe_name.toEditable()
         val view = binding.root
+
         setContentView(view)
     }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) hideSystemUI()
+    }
+
+    private fun hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                // Set the content to appear under the system bars so that the
+                // content doesn't resize when the system bars hide and show.
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                // Hide the nav bar and status bar
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    // Shows the system bars by removing all the flags
+// except for the ones that make the content appear under the system bars.
+    private fun showSystemUI() {
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+    }
+
     fun update(){
         _binding = ShowRecipeBinding.inflate(layoutInflater)
         if (Recipe_to_load.preview_image != null) {
